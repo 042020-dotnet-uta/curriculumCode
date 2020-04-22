abe28306da7c0880b82d62df35f9af76f32b48b1
Mark Moore <mark.moore@revature.com>
2020-04-22 17:40:49 -0500

    adds D3 Rock/Paper/Scissors Project

---
+++ Week1/RPSGame/RPS_Game/RPS_Game/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPS_Game
+{
+    class Game
+    {
+        public List<Round> Rounds { get; set; }
+        public Player p1 { get; set; }
+        public Player p2 { get; set; }
+    }
+}
