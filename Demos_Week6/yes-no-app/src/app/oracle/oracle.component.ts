b3eb84200e808768a322bc1a280c82bfc8dcbb46
Nick Escalona <nicholas.escalona@gmail.com>
2020-05-29 15:29:31 -0500

    promises/observables

--- Demos_Week6/yes-no-app/src/app/oracle/oracle.component.ts
+++ Demos_Week6/yes-no-app/src/app/oracle/oracle.component.ts
@@ -15,6 +15,7 @@ export class OracleComponent implements OnInit {
   getAnswer() {
     this.yesNo.getAnswer()
       .then(answer => this.answer = answer);
+    console.log(this.answer);
   }
 
   ngOnInit(): void {
@@ -22,3 +23,11 @@ export class OracleComponent implements OnInit {
   }
 
 }
+// promises:
+//    only one result ever
+//    only one line of code can wait on the promise at a time
+//    standard in ES6
+// observables:
+//    supports many results arriving at different times into the future forever
+//    many subscribers to the same observable
+//    not standard JS, provided by package rxjs.
